c8b0bfd4faa92f7f65813bbd2a8ce8875d609935
Your Name <aminvatsal1999@gmail.com>
2024-12-19 02:31:09 -0600

    client/server dont work

--- general_manager/src/main.rs
+++ general_manager/src/main.rs
@@ -1,14 +1,14 @@
-use player::Player;
 use tokio::time::{sleep, Duration};
+use pssstt::common::messages::ServiceID;
 
 mod player;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    let player = Player::new("Gibbs".to_owned(), "Lions".to_owned());
-
+    let sid = ServiceID::new("general manager".to_owned(), "generates a team".to_owned());
+    let client = pssstt::client::Client::new(sid, "locker_room:24017");
     loop {
-        println!("Player: {:?}", player);
+        client.send_message("Give me something".to_owned());
         sleep(Duration::from_millis(100)).await;
     }
 }
--- locker_room/src/main.rs
+++ locker_room/src/main.rs
@@ -1,9 +1,9 @@
 use mongodb::{options::ClientOptions, Client};
 use std::env;
+use pssstt::common::messages::{Message, ServiceID};
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    println!("HELLO WORLD!");
     let uri = env::var("MONGODB_URI").expect("MONGODB_URI must be set! Use docker-compose!");
 
     let mut client_options = ClientOptions::parse(uri).await?;
@@ -12,8 +12,21 @@ async fn main() -> Result<(), anyhow::Error> {
 
     let client = Client::with_options(client_options)?;
 
+
     let database_names = client.list_database_names(None, None).await?;
 
     println!("Databases: {:?}", database_names);
+
+    // Create PSSSTT server
+    let sid = ServiceID::new("general manager".to_owned(), "generates a team".to_owned());
+    let server = pssstt::server::Server::new(sid, "0.0.0.0:24017");
+
+    let handle_fn = |msg: String| {
+        let des_msg = serde_json::from_str::<Message>(&msg).unwrap();
+        println!("Incoming msg: {:?}", des_msg);
+        return String::from("Testing");
+    };
+    pssstt::server::Server::start(server, handle_fn);
+    
     Ok(())
 }
